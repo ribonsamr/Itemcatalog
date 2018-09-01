437a9951b76fdae1c75a64faf9c51f771307c199
Amr <amressam01@hotmail.com>
2018-09-02 01:21:22 +0200

    working login/logout/signup

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-from flask import Flask
-from flask import flash, redirect, render_template, request, session, abort, url_for
+from os import urandom
 
-import os
+from flask import Flask, flash, redirect, render_template, \
+request, session, abort, url_for
+
+from sqlalchemy.orm import sessionmaker
+from db import *
+
+engine = create_engine('sqlite:///main.db', echo=True)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).all()
+
     if not session.get('loggedin'):
-        return "<a href='login'>login</a>"
+        return render_template('index.html', logged=False, data=query)
     else:
-        return "<a href='logout'>logout</a>"
+        return render_template('index.html', logged=True, data=query)
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        if not session.get("loggedin"):
+            return render_template("signup.html")
+        else:
+            return redirect(url_for("index"))
+    else:
+        if not session.get("loggedin"):
+            user, password = request.form['username'], request.form['password']
+            if not user or not password:
+                flash('Invalid input')
+                return redirect(url_for("signup"))
+            else:
+                Session = sessionmaker(bind=engine)
+                s = Session()
+                user = User(user, password)
+                s.add(user)
+                s.commit()
+                session['loggedin'] = True
+
+                return redirect(url_for("index"))
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        if request.form['password'] == '123' and request.form['username'] == 'amr':
+        username = request.form['username']
+        password = request.form['password']
+
+        Session = sessionmaker(bind=engine)
+        s = Session()
+
+        query = s.query(User).filter(User.username.in_([username]),
+                                     User.password.in_([password]))
+        result = query.first()
+
+        if result:
             session['loggedin'] = True
         else:
-            flash('wrong password')
-        return redirect(url_for('index'))
+            flash('wrong username or password: %s' %(username))
+
+        return redirect(url_for('login'))
     else:
         if not session.get('loggedin'):
             return render_template('login.html')
@@ -36,5 +79,5 @@ def not_found(error):
     return render_template('error.html'), 404
 
 
-app.secret_key = sKey = os.urandom(12)
+app.secret_key = sKey = urandom(12)
 app.run(host='0.0.0.0', debug=True)

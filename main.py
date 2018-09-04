2fc18b656cc5ff11a5a0776e1de5ebc266012d90
Amr <amressam01@hotmail.com>
2018-09-04 08:51:36 +0200

    automatically serve static with variable http arg to update browser cache

--- main.py
+++ main.py
@@ -395,5 +395,24 @@ def api_view_users_all():
     return jsonify(Users=[i.serialize for i in query])
 
 
+"""
+    Automatically update static files
+    http://flask.pocoo.org/snippets/40/
+"""
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

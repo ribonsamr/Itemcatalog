de47604ca36e611dd6d0c3869cdf9c9d7b8ef489
Amr <amressam01@hotmail.com>
2018-09-08 17:23:07 +0200

    refactoring, finished signup methods

--- static/base.js
+++ static/base.js
@@ -1,39 +1,62 @@
-function getItems(func) {
-  $.ajax({
-    type: 'GET',
-    url: '/api/items',
-    processData: false,
-    data: true,
-    dataType: 'json',
-    success: function(result) {
-      func(result);
-    }
-  });
-}
-
 function mainViewModel() {
   this.loggedIn = ko.observable(false);
   this.content = ko.observableArray([]);
   this.navItems = [
-    { title: ko.observable('Home'), url: ko.observable('/'), visib: true },
-    { title: ko.observable('Items'), visib: true },
-    { title: ko.observable('API') , visib: true},
-    { title: ko.observable('Users'), visib: ko.pureComputed(function() {
-        return this.loggedIn();
-    }, this) },
-    { title: ko.observable('Login'), visib: ko.pureComputed(function() {
-        return !this.loggedIn();
-    }, this) },
-    { title: ko.observable('Logout'), visib: ko.pureComputed(function() {
-        return this.loggedIn();
-    }, this) },
+    { title: ko.observable('Home'),
+      url: ko.observable('/'),
+      visib: true
+    },
+
+    { title: ko.observable('Items'),
+      url: ko.observable('/items'),
+      visib: true
+    },
+
+    { title: ko.observable('API'),
+      url: ko.observable('/api'),
+      visib: true
+    },
+
+    { title: ko.observable('Users'),
+      url: ko.observable('/users'),
+      visib: ko.pureComputed(function() { return this.loggedIn(); }, this)
+    },
+
+    { title: ko.observable('Login'),
+      url: ko.observable('/login'),
+      visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)
+    },
+
+    { title: ko.observable('Signup'),
+      url: ko.observable('/signup'),
+      visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)
+    },
+
+    { title: ko.observable('Logout'),
+      url: ko.observable('/logout'),
+      visib: ko.pureComputed(function() { return this.loggedIn(); }, this)
+    },
   ];
+
+  this.getItems = function(func) {
+    $.ajax({
+      type: 'GET',
+      url: '/api/items',
+      processData: false,
+      data: true,
+      dataType: 'json',
+      success: function(result) {
+        func(result);
+      }
+    });
+  };
+
 }
 
 function formModel() {
-  this.userName = ko.observable("");
-  this.userEmail = ko.observable("");
-  this.userPassword = ko.observable("");
+  this.username = ko.observable("");
+  this.email = ko.observable("");
+  this.password = ko.observable("");
   this.submitData = function() {
     $.ajax({
       type: 'POST',
@@ -41,9 +64,19 @@ function formModel() {
       processData: false,
       data: JSON.stringify(ko.toJSON(formViewModel, null, 2)),
       contentType: 'application/json',
-      success: function(result) {
-        alert(result);
-        // do some stuff here
+      success: function(xhr, msg) {
+        mainViewModel.loggedIn(true);
+      },
+      error: function(xhr, msg, error) {
+        if (xhr.status === 400) {
+          location.reload();
+        }
+        if (xhr.status === 302) {
+          window.location.href = xhr.responseText;
+        }
+        if (xhr.status === 405) {
+          alert(xhr.responseText)
+        }
       }
     });
   };

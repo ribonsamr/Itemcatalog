1c546f739a546ac103626c2ce08733dfb179c61c
Amr <amressam01@hotmail.com>
2018-09-09 07:14:41 +0200

    refactoring my viewmodels, now theyre prettirirer

--- static/base.js
+++ static/base.js
@@ -1,53 +1,21 @@
 function mainViewModel() {
   this.loggedIn = ko.observable(false);
   this.content = ko.observableArray([]);
-  this.navItems = [
-    { title: ko.observable('Users'),
-      url: ko.observable('/users'),
-      visib: ko.pureComputed(function() { return this.loggedIn(); }, this)
-    },
+  this.navItems = [{ title: ko.observable('Users'),
+  url: ko.observable('/users'),
+  visib: ko.pureComputed(function() { return this.loggedIn(); }, this)},
 
-    { title: ko.observable('Login'),
-      url: ko.observable('/login'),
-      visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)
-    },
+  { title: ko.observable('Login'),
+  url: ko.observable('/login'),
+  visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)},
 
-    { title: ko.observable('Signup'),
-      url: ko.observable('/signup'),
-      visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)
-    }];
+  { title: ko.observable('Signup'),
+  url: ko.observable('/signup'),
+  visib: ko.pureComputed(function() { return !this.loggedIn(); }, this)}];
 
   this.logoutButton = {
     title: ko.observable('Logout'),
     visib: ko.pureComputed(function() { return this.loggedIn(); }, this),
-
-    logOut: function() {
-      $.ajax({
-        type: 'POST',
-        url: '/logout',
-        processData: false,
-        contentType: 'application/json',
-        success: function(xhr, msg) {
-          var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-              console.log('User signed out.');
-            });
-          mainViewModel.loggedIn(false);
-          window.location.href = '/';
-        },
-        error: function(xhr, msg, error) {
-          if (xhr.status === 400) {
-            location.reload();
-          }
-          if (xhr.status === 302) {
-            window.location.href = xhr.responseText;
-          }
-          if (xhr.status === 405) {
-            alert(xhr.responseText)
-          }
-        }
-      });
-    }
   }
 
   this.getItems = function(func) {
@@ -69,36 +37,14 @@ function formModel() {
   this.username = ko.observable("");
   this.email = ko.observable("");
   this.password = ko.observable("");
-  this.submitData = function() {
-    $.ajax({
-      type: 'POST',
-      url: '/signup',
-      processData: false,
-      data: JSON.stringify(ko.toJSON(formViewModel, null, 2)),
-      contentType: 'application/json',
-      success: function(xhr, msg) {
-        mainViewModel.loggedIn(true);
-        window.location.href = '/';
-      },
-      error: function(xhr, msg, error) {
-        if (xhr.status === 400) {
-          location.reload();
-        }
-        if (xhr.status === 302) {
-          window.location.href = xhr.responseText;
-        }
-        if (xhr.status === 405) {
-          alert(xhr.responseText)
-        }
-      }
-    });
-  };
+}
 
-  this.logIn = function() {
+
+function auth() {
+  this.login = function() {
     $.ajax({
       type: 'POST',
       url: '/login',
-      processData: false,
       data: JSON.stringify(ko.toJSON(formViewModel, null, 2)),
       contentType: 'application/json',
       success: function(xhr, msg) {
@@ -118,12 +64,67 @@ function formModel() {
       }
     });
   }
+
+  this.submitData = function() {
+    $.ajax({
+      type: 'POST',
+      url: '/signup',
+      data: JSON.stringify(ko.toJSON(formViewModel, null, 2)),
+      contentType: 'application/json',
+      success: function(xhr, msg) {
+        mainViewModel.loggedIn(true);
+        window.location.href = '/';
+      },
+      error: function(xhr, msg, error) {
+        if (xhr.status === 400) {
+          location.reload();
+        }
+        if (xhr.status === 302) {
+          window.location.href = xhr.responseText;
+        }
+        if (xhr.status === 405) {
+          alert(xhr.responseText)
+        }
+      }
+    });
+  }
+
+  this.logout = function() {
+    $.ajax({
+      type: 'POST',
+      url: '/logout',
+      processData: false,
+      contentType: 'application/json',
+      success: function(xhr, msg) {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+        mainViewModel.loggedIn(false);
+        window.location.href = '/';
+      },
+      error: function(xhr, msg, error) {
+        if (xhr.status === 400) {
+          location.reload();
+        }
+        if (xhr.status === 302) {
+          window.location.href = xhr.responseText;
+        }
+        if (xhr.status === 405) {
+          alert(xhr.responseText)
+        }
+      }
+    });
+  }
 }
+
 
 var masterViewModel = (function() {
   this.mainViewModel = new mainViewModel();
   this.formViewModel = new formModel();
+  this.auth = new auth();
 })();
+
 
 $(function() {
   ko.applyBindings(masterViewModel);

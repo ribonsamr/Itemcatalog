281bd701c9bba36ba264ac8947432aca87baffff
Amr <amressam01@hotmail.com>
2018-09-09 13:41:04 +0200

    comment

--- static/base.js
+++ static/base.js
@@ -51,7 +51,7 @@ function mainViewModel() {
       for (var i = 0; i < result.length; i++) {
         mainViewModel.content.push(result[i])
       }
-      console.log(mainViewModel.content());
+      // console.log(mainViewModel.content());
     });
   };
 

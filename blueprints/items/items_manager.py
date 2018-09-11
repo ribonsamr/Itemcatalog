27d4762e206f7a0cfb1b21901e07eaeaa5463c89
Amr <amressam01@hotmail.com>
2018-09-11 19:17:00 +0200

    commenting: items_manager.py

--- blueprints/items/items_manager.py
+++ blueprints/items/items_manager.py
@@ -9,16 +9,19 @@ from models import Item, db
 
 items_manager = Blueprint('/items', __name__)
 
+# init Flask-uploads
 photos = UploadSet('photos', IMAGES)
 
 
-@items_manager.route('/<catagory>/<name>')
-def view_by_data(catagory, name):
-    query = Item.query.filter(Item.name.ilike(name),
-                              Item.catagory.ilike(catagory)).first()
-    return render_template('view.html', query=query)
+# @items_manager.route('/<catagory>/<name>')
+# def view_by_data(catagory, name):
+#     query = Item.query.filter(Item.name.ilike(name),
+#                               Item.catagory.ilike(catagory)).first()
+#     return render_template('view.html', query=query)
 
 
+# A POST request will parse form data to add a new item.
+# An image is not required, and a record can be added without it.
 @items_manager.route("/add", methods=['POST'])
 @login_required
 def add():
@@ -26,38 +29,45 @@ def add():
     item_catagory = request.form.get('catagory')
     item_file = ''
 
+    # check if a file was included
     if 'file' in request.files:
-        print(request.files['file'])
+        # print(request.files['file'])
         item_file = request.files['file']
 
+    # block the request if fields are missing
     if not item_name or not item_catagory:
         return "Missing fields.", 405
 
     query = Item.query.filter(Item.name.ilike(item_name),
                               Item.catagory.ilike(item_catagory)).first()
 
+    # if a query of the same data exists
     if query:
         return "Already exists", 405
 
+    # if a file was included, save it and link it to the item in the db.
     if item_file:
         filename = photos.save(item_file)
-        # return (filename, redirect(url_for('image_get', path=filename)))
         db.session.add(Item(item_name, item_catagory, filename))
         db.session.commit()
         return "OK", 200
 
+    # add the item to the db
     db.session.add(Item(item_name, item_catagory, ''))
     db.session.commit()
 
     return "OK", 200
 
 
+# A POST will receive a filename of a record, and will return a full path
+# to the image.
 @items_manager.route('/image', methods=['POST'])
 def get_image():
     file_url = photos.url(request.values['filename'])
     return file_url
 
 
+# A POST request will get JSON data about a record to delete.
 @items_manager.route('/delete', methods=['POST'])
 @login_required
 def delete():
@@ -72,16 +82,19 @@ def delete():
 
     item = query.first()
 
+    # check if the item has an image linked to it, then delete it.
     if item.image_filename:
         file_path = photos.path(item.image_filename)
         os.remove(file_path)
 
+    # then delete the item
     query.delete(synchronize_session=False)
     db.session.commit()
 
     return "OK", 200
 
 
+# A POST request will receive a JSON data with the modified record to commit.
 @items_manager.route('/edit', methods=['POST'])
 @login_required
 def edit():
@@ -91,11 +104,14 @@ def edit():
     item_name = j_data['name']
     item_catagory = j_data['catagory']
 
+    # use the id to find the item in the db
     query = Item.query.filter(Item.id == item_id)
 
+    # if the item doesn't exists, block the request
     if not query:
         return "Not found", 404
 
+    # modify the record and commit it.
     query = query.first()
     query.name = item_name
     query.catagory = item_catagory

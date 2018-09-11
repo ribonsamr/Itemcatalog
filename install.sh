f98568479921ca7d609873f792767602787b6f35
Amr <amressam01@hotmail.com>
2018-09-11 13:40:58 +0200

    Install comments

--- install.sh
+++ install.sh
@@ -4,17 +4,18 @@ if [[ $1 = -s ]]; then
   exit 1
 fi
 
-forcemigrate=false
-
 # install python required packages
 echo -e '-- Installing required packages --\n'
-pip3 install --upgrade flask-migrate flask-script flask-sqlalchemy flask-wtf
-pip3 install --upgrade psycopg2-binary flask-login flask-uploads google-auth requests
-pip3 install --upgrade oauth2client google-api-python-client
+pip3 install --upgrade flask-migrate flask-script flask-sqlalchemy flask-wtf \
+psycopg2-binary flask-login flask-uploads google-auth requests \
+oauth2client google-api-python-client
 
-# create postgresql database
+
+# create the postgresql database
 echo -e '\n-- Creating the database --\n'
 
+
+forcemigrate=false
 create_database() {
   if [[ $1 = true ]]; then
     psql -c 'drop database itemcatag_db'
@@ -23,19 +24,26 @@ create_database() {
   forcemigrate=true
 }
 
+
+# check if the db already exists
 if psql -lqt | cut -d \| -f 1 | grep -qw 'itemcatag_db'; then
   printf "Database exists. Do you want to re-create it? (y/n): "
   read answer
   if [[ "${answer,,}" = 'y' ]]; then
+
+    # delete the old db and recreate a new one
     create_database true
   fi
 else
+
+  # no db exists, create a new one.
   create_database
 fi
 
 
 # init the database
 echo -e '\n-- Initializing the database --\n'
+
 
 migrate() {
   if [[ $1 = true ]]; then
@@ -48,14 +56,18 @@ migrate() {
 }
 
 migrations="migrations"
+
+# check if the migrations folder already exists
 if [ -d "${migrations}" ]; then
   if [[ $forcemigrate = true ]]; then
+    # if a new db was created earlier, force migrations.
     migrate true
 
   else
     printf "Migrations directory already exists, do you want to remove it? (y/n): "
     read answer
     if [[ "${answer,,}" = 'y' ]]; then
+      # if the user agrees, delete the old one, and remigrate.
       migrate true
     else
       echo "Won't delete the migrations folder."

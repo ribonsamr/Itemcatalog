992b6214d9227f86ae29e5e6b46fa27673ab0f77
Amr <amressam01@hotmail.com>
2018-09-11 14:08:17 +0200

    commenting: main.py

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ from models import db, User, Item
 
 app = Flask(__name__)
 
+# load flask config file
 app.config.from_pyfile  ('config/config.py')
+
+# init SQLAlchemy
 db.init_app             (app)
+
+# init CSRFProtect
 csrf.init_app           (app)
+
+# init login_manager
 login_manager.init_app  (app)
+
+# init flask_uploads
 configure_uploads       (app, (photos))
 
+# register the blueprints [api, auth, items_manager]
 app.register_blueprint  (api)
 app.register_blueprint  (auth)
 app.register_blueprint  (items_manager)
@@ -37,21 +47,23 @@ def main():
 def items():
     return render_template('items.html')
 
-@app.route('/test', methods=['POST'])
-def test():
-    form = request.form
-    if 'file' in request.files:
-        print(request.files['file'])
-    return "OK"
+
+# for testing purposes
+# @app.route('/test', methods=['POST'])
+# def test():
+#     form = request.form
+#     if 'file' in request.files:
+#         print(request.files['file'])
+#     return "OK"
 
 
 # @app.errorhandler(404)
 # def not_found(error):
 #     return render_template('error.html'), 404
 
+
 # Automatically update static files
 # http://flask.pocoo.org/snippets/40/
-
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
@@ -65,13 +77,16 @@ def dated_url_for(endpoint, **values):
                                  endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
+# ========================================================
 
-
+# pass the current_user authentication state always to
+# the front-end (Knockout.JS)
 @app.context_processor
 def current_user_authed():
     return dict(current_user_authed=str(current_user.is_authenticated).lower())
 
 
 if __name__ == '__main__':
-    # app.run(ssl_context=('config/cert.pem', 'config/key.pem'))
+    # uncomment next line to enable HTTPS
+    # app.run('0.0.0.0', ssl_context=('config/cert.pem', 'config/key.pem'))
     app.run('0.0.0.0', port=5000)

5194e04e913f31f04ca9a10078e6f10e1bdbe9c8
Amr <amressam01@hotmail.com>
2018-09-11 13:54:00 +0200

    pip3 uninstall -y

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@ if [[ $1 = -s ]]; then
   ./uninstall.sh 2> /dev/null
   exit 1
 fi
-pip3 uninstall flask flask-migrate flask-script flask-sqlalchemy flask-wtf \
+pip3 uninstall -y flask flask-migrate flask-script flask-sqlalchemy flask-wtf \
 psycopg2-binary flask-login flask-uploads google-auth requests \
 oauth2client google-api-python-client
 psql -c 'drop database itemcatag_db'
